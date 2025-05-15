bb16d88e1bafd5011f30eb00b7b8a891910d5a4c
Lucia Brammer <brammerl96@gmail.com>
2025-05-14 21:15:12 -0700

    chore: Baseline error handling

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -2,8 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const product = require("./routes/product.js");
+const { Prisma } = require("@prisma/client");
+const {
+  PrismaClientValidationError,
+} = require("@prisma/client/runtime/library");
 
 app.use(express.json());
+
+app.use((err, req, res, next) => {
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    return res.status(400).json({ error: err.message });
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    // Handle common Prisma errors (e.g., unique constraint violation)
+    if (err.code === "P2002") {
+      return res
+        .status(400)
+        .json({ error: "A unique constraint violation occurred." });
+    }
+  }
+
+  res.status(500).json({ error: "Internal Server Error" });
+});
 
 app.get("/", (req, res) => {
   res.send("hello world 3");
