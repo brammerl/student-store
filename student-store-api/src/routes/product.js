100c1033dd61d8898e9600fb0267297f605de5ee
Lucia Brammer <brammerl96@gmail.com>
2025-05-14 19:58:59 -0700

    chore: Create delete route

--- student-store-api/src/routes/product.js
+++ student-store-api/src/routes/product.js
@@ -3,10 +3,21 @@ const express = require("express");
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+// Get all product
+router.get("/", async (_, res) => {
   const products = await prisma.product.findMany();
 
   res.json(products);
+});
+
+// Delete product by id
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await prisma.product.delete({
+    where: { id: parseInt(id) },
+  });
+
+  res.json(deletedProduct);
 });
 
 module.exports = router;
