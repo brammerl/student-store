0862d0d7fe00c54cf031bff32e044beb49e09251
Lucia Brammer <brammerl96@gmail.com>
2025-05-16 14:38:03 -0700

    chore: Refactoring

--- student-store-api/src/routes/orderItem.js
+++ student-store-api/src/routes/orderItem.js
@@ -5,13 +5,13 @@ const orderItemController = require("../controllers/orderItemController");
 // Get all order items
 router.get("/", orderItemController.getAllOrderItems);
 
+// Create a new order item
+router.post("/", orderItemController.createOrderItem);
+
 // Get an order item by id
 router.get("/:id", orderItemController.getOrderItemById);
 
 // Delete an order item by id
 router.put("/:id", orderItemController.updateOrderItemById);
-
-// Create a new order item
-router.post("/:id", orderItemController.createOrderItem);
 
 module.exports = router;
