6746f86e9deb27f6fc143f5d52d4c4cfcca21d76
Lucia Brammer <brammerl96@gmail.com>
2025-05-16 14:23:43 -0700

    chore: Add comments

--- student-store-api/src/routes/orderItem.js
+++ student-store-api/src/routes/orderItem.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const orderItemController = require("../controllers/orderItemController");
 
+// Get all order items
 router.get("/", orderItemController.getAllOrderItems);
 
+// Get an order item by id
 router.get("/:id", orderItemController.getOrderItemById);
 
+// Delete an order item by id
 router.put("/:id", orderItemController.updateOrderItemById);
 
+// Create a new order item
 router.post("/:id", orderItemController.createOrderItem);
 
 module.exports = router;
