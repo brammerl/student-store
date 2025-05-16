d4d0607c5e4c4cb4fbf6cc6221b3b5e81895fc58
Lucia Brammer <brammerl96@gmail.com>
2025-05-15 20:20:11 -0700

    chore: Add comment

--- student-store-api/src/routes/product.js
+++ student-store-api/src/routes/product.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController.js");
 
-// Get all product
+// Get all products, filter logic applied if provided
 router.get("/", productController.getProducts);
 
 // Delete product by id
