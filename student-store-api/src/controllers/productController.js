b9241dc5da583ca8c0ed2f1a443df0cc5ac8332b
Lucia Brammer <brammerl96@gmail.com>
2025-05-15 19:44:02 -0700

    chore: Move functions into controllers

---
+++ student-store-api/src/controllers/productController.js
@@ -0,0 +1,71 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { createFilterAndSortObj } = require("../helpers/helpers.js");
+
+const getProducts = async (req, res) => {
+  const queryParams = req.query;
+
+  const filters = createFilterAndSortObj(queryParams);
+
+  const products = await prisma.product.findMany({
+    ...filters,
+  });
+
+  res.json(products);
+};
+
+const deleteProductById = async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await prisma.product.delete({
+    where: { id: parseInt(id) },
+  });
+
+  res.json(deletedProduct);
+};
+
+const updateProductById = async (req, res) => {
+  const { id } = req.params;
+
+  const fetchedProduct = await prisma.product.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!fetchedProduct) {
+    return res.status(404).json({ error: "No product found" });
+  }
+
+  const { data } = req.body;
+
+  const updatedProduct = await prisma.product.update({
+    where: { id: parseInt(id) },
+    data: {
+      ...fetchedProduct,
+      ...data,
+    },
+  });
+
+  res.json(updatedProduct);
+};
+
+const createProduct = async (req, res) => {
+  const { data } = req.body;
+
+  if (!data.length) {
+    return res.status(400).json({ error: "No products provided" });
+  }
+
+  const productsCreated = await prisma.product.createManyAndReturn({
+    data,
+  });
+
+  res.json(productsCreated);
+};
+
+module.exports = {
+  getProducts,
+  deleteProductById,
+  updateProductById,
+  createProduct,
+};
--- student-store-api/src/routes/product.js
+++ student-store-api/src/routes/product.js
@@ -2,71 +2,18 @@ const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const prisma = new PrismaClient();
 const router = express.Router();
-const { createFilterAndSortObj } = require("../helpers/helpers.js");
+const productController = require("../controllers/productController.js");
 
 // Get all product
-router.get("/", async (req, res) => {
-  const queryParams = req.query;
-
-  const filters = createFilterAndSortObj(queryParams);
-
-  const products = await prisma.product.findMany({
-    ...filters,
-  });
-
-  res.json(products);
-});
+router.get("/", productController.getProducts);
 
 // Delete product by id
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const deletedProduct = await prisma.product.delete({
-    where: { id: parseInt(id) },
-  });
-
-  res.json(deletedProduct);
-});
+router.delete("/:id", productController.deleteProductById);
 
 // Update by ID
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const fetchedProduct = await prisma.product.findUnique({
-    where: {
-      id: parseInt(id),
-    },
-  });
-
-  if (!fetchedProduct) {
-    return res.status(404).json({ error: "No product found" });
-  }
-
-  const { data } = req.body;
-
-  const updatedProduct = await prisma.product.update({
-    where: { id: parseInt(id) },
-    data: {
-      ...fetchedProduct,
-      ...data,
-    },
-  });
-
-  res.json(updatedProduct);
-});
+router.put("/:id", productController.updateProductById);
 
 // Create product generic allows for multiple entries
-router.post("/", async (req, res) => {
-  const { data } = req.body;
-
-  if (!data.length) {
-    return res.status(400).json({ error: "No products provided" });
-  }
-
-  const productsCreated = await prisma.product.createManyAndReturn({
-    data,
-  });
-
-  res.json(productsCreated);
-});
+router.post("/", productController.createProduct);
 
 module.exports = router;
